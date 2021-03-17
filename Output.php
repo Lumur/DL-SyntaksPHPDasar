9ac43c2434a7c6a603b21b1444ac9adc3b36ee69
Deepleo <64431154+Lumur@users.noreply.github.com>
2021-03-17 15:29:07 +0800

    Delete Output.php

--- Output.php
+++
@@ -1,4 +0,0 @@
-echo "Ini adalah contoh echo <br>";
-print " Ini adalah contoh print. <i>hasilnya tidak jauh beda dengan echo</i> <br>";
-print_r ("print_r digunakan untuk mencetak hasil array, selain itu perbedaannya terletak pada penulisannya yaitu dengan menambahkan tanda buka dan tutup kurung <br>");
-var_dump("ini adalah contoh var_dump yang fungsinya untuk memperlihatkan banyak karakter dan tipe datanya <br>");
